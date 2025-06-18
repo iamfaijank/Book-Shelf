5057bc829a2cb1644d66213a112a34a7ce4e12e5
iamfaijank <iamfaijankq@gmail.com>
2025-06-18 16:51:26 +0530

    feat: Initialize App

--- expense_management/hooks.py
+++ expense_management/hooks.py
@@ -1,6 +1,6 @@
 app_name = "expense_management"
 app_title = "Expense Management"
-app_publisher = "✔️ Publisher: rishabh"
+app_publisher = "rishabh"
 app_description = "This app is for money management"
 app_email = "rarishab893@gmail.com"
 app_license = "mit"
